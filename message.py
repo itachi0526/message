10f5969bdf5fcad9163e458a8ba798880d370377
Eric Haung <fiser0317@gmail.com>
2021-06-05 14:11:41 +0800

    calc avg

--- message.py
+++ message.py
@@ -6,3 +6,9 @@ with open('001.txt', 'r') as f: # 打開檔案，使用讀取模式
         count += 1 # 每讀取一筆資料count + 1
         if count % 3 == 0: # 每數到3的倍數
             print(len(data)) # 就print
+print('檔案讀取完畢，總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print('留言平均長度是', sum_len/len(data))    
